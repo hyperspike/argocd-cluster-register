e505b9598149471e794ff0f8e7ec61b743ffe7f1
Dan Molik <dan@highline.co>
2024-01-01 10:15:11 -0800

    start prepwork on VPC provisioning

---
+++ vpc/aws/aws.go
@@ -0,0 +1,55 @@
+package aws
+
+type VPC struct {
+	client string
+}
+
+func (v *VPC) createVPC() {
+
+}
+
+func (v *VPC) destroyVPC() {
+
+}
+
+func (v *VPC) createNat() {
+
+}
+
+func (v *VPC) destroyNat() {
+
+}
+
+func (v *VPC) createSubnet() {
+
+}
+
+func (v *VPC) destroySubnet() {
+
+}
+
+func (v *VPC) createVPCPeer() {
+
+}
+
+func (v *VPC) disconnectVPCPeer() {
+
+}
+
+func (v *VPC) createFirewall() {
+}
+
+func (v *VPC) destroyFirewall() {
+
+}
+
+type FirewallRule struct {
+}
+
+func (v *VPC) createFirewallRule() {
+
+}
+
+func (v *VPC) destroyFirewallRule() {
+
+}
